17f61bc600f31f09c9e3741960acab3b0ff65853
Ariella1997 <108226376+Ariella1997@users.noreply.github.com>
2022-11-23 20:22:43 +0000

    Java Projects

---
+++ Java Projects/KnotCrosses/src/Main.java
@@ -0,0 +1,10 @@
+
+// I built this quite early on in my Java learning. So please expect lots of things will need to be updated. 
+public class Main {
+    public static void main(String[] args){
+
+        KnotCrosses f1 = new KnotCrosses() ;
+        f1.MainMenu() ; 
+
+    }
+}
