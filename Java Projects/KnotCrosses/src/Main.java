5631766c810c1fbd7975de0588c00ecac4f290c5
Ariella1997 <108226376+Ariella1997@users.noreply.github.com>
2023-02-14 10:23:50 +0000

    Cleaning

--- Java Projects/KnotCrosses/src/Main.java
+++
@@ -1,9 +0,0 @@
-// I built this quite early on in my Java learning. So please expect lots of things will need to be updated. 
-public class Main {
-    public static void main(String[] args){
-
-        KnotCrosses f1 = new KnotCrosses() ;
-        f1.MainMenu() ; 
-
-    }
-}
